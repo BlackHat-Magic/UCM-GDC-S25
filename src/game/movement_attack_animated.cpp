000b8ec07d61d521bebcabe71ee59d8911efa5ff
Sofia Langer-Osuna <sofialangerosuna@gmail.com>
2025-04-12 03:20:54 -0700

    Refactor `Player` to be as discussed in DMs

---
+++ src/game/movement_attack_animated.cpp
@@ -0,0 +1,83 @@
+#include "movement_attack_animated.h"
+
+MovementAttackAnimated::MovementAttackAnimated(SDL_Renderer* renderer, const char* sprite_path, int sprite_width, int sprite_height, float x, float y, int** animations, float animation_speed, float movement_speed)
+    : Entity(renderer, sprite_path, sprite_width, sprite_height, x, y, animations),
+      animationSpeed(animation_speed),
+      movementSpeed(movement_speed),
+      direction(NONE),
+      isAttacking(false) {
+    setAnimation(0);
+    setStage(0);
+    lastAnimationTime = 0.0f;
+}
+
+void MovementAttackAnimated::update(float time, float deltaTime) {
+    MovementDirection newDirection = control(time, deltaTime);
+
+    if (isAttacking) {
+        if (time - lastAnimationTime >= animationSpeed) {
+            lastAnimationTime = time;
+            if (advanceAnimation()) {
+                isAttacking = false;
+                setAnimation(0);
+
+            }
+        }
+
+        control(time, deltaTime);
+
+        return;
+    }
+
+    direction = newDirection;
+
+    if (direction != NONE) {
+        setAnimation(1);
+
+        switch (direction) {
+            case UP:    y -= movementSpeed * deltaTime; break;
+            case DOWN:  y += movementSpeed * deltaTime; break;
+            case LEFT:  x -= movementSpeed * deltaTime; break;
+            case RIGHT: x += movementSpeed * deltaTime; break;
+            case UP_LEFT:   x -= movementSpeed * deltaTime; y -= movementSpeed * deltaTime; break;
+            case UP_RIGHT:  x += movementSpeed * deltaTime; y -= movementSpeed * deltaTime; break;
+            case DOWN_LEFT: x -= movementSpeed * deltaTime; y += movementSpeed * deltaTime; break;
+            case DOWN_RIGHT: x += movementSpeed * deltaTime; y += movementSpeed * deltaTime; break;
+            default: break;
+        }
+    } else {
+        setAnimation(0);
+    }
+
+    if (time - lastAnimationTime >= animationSpeed) {
+        lastAnimationTime = time;
+        advanceAnimation();
+    }
+
+    setPosition(static_cast<int>(x), static_cast<int>(y));
+}
+
+void MovementAttackAnimated::attack(float time) {
+    if (!isAttacking) {
+        isAttacking = true;
+        setAnimation(2); 
+        setStage(0);
+        lastAnimationTime = time;
+    }
+}
+
+void MovementAttackAnimated::setAnimationSpeed(float speed) {
+    animationSpeed = speed;
+}
+
+void MovementAttackAnimated::setMovementSpeed(float speed) {
+    movementSpeed = speed;
+}
+
+float MovementAttackAnimated::getAnimationSpeed() const {
+    return animationSpeed;
+}
+
+float MovementAttackAnimated::getMovementSpeed() const {
+    return movementSpeed;
+}
---
+++ src/game/movement_attack_animated.h
@@ -0,0 +1,34 @@
+#include "entity.h"
+
+enum MovementDirection {
+    UP,
+    UP_LEFT,
+    UP_RIGHT,
+    DOWN,
+    DOWN_LEFT,
+    DOWN_RIGHT,
+    LEFT,
+    RIGHT,
+    NONE
+};
+
+// animations[0] = idle, animations[i] = movement, animations[2] = attack
+class MovementAttackAnimated : public Entity {
+public:
+    MovementAttackAnimated(SDL_Renderer* renderer, const char* sprite_path, int sprite_width, int sprite_height, float x, float y, int** animations, float animation_speed, float movement_speed);
+
+    void update(float time, float deltaTime) override;
+    virtual MovementDirection control(float time, float deltaTime) = 0;
+    void attack(float time);
+    void setAnimationSpeed(float speed);
+    void setMovementSpeed(float speed);
+    float getAnimationSpeed() const;
+    float getMovementSpeed() const;
+
+private:
+    float lastAnimationTime;
+    float animationSpeed;
+    float movementSpeed;
+    MovementDirection direction;
+    bool isAttacking; // used to know not to change back to idle/movement animation during attack
+};
